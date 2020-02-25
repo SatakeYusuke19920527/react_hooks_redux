875ee007660b65100eecbe5305b8e8cafce52ce0
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-02-25 09:50:36 +0900

    Hooksのカウントアプリ実装

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App () {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
+
+  const increment = () => {
+    setCount(count + 1)
+    setText('plus 1 (oAo)')
+  }
+
+  const increment2 = () => {
+    setCount(counting => {
+      setCount(counting + 1)
+    })
+    setText('increment2')
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+    setText('minus 1 (oAo)')
+  }
+
+  const decrement2 = () => {
+    setCount(counting => {
+      setCount(counting - 1)
+    })
+    setText('decrement2')
+  }
+
+  const reset = () => {
+    setCount(0)
+  }
+
+  const multiple = () => {
+    setCount(num => num * 2)
+  }
+
+  const three = () => {
+    setCount(previous => {
+      return previous % 3 === 0 ? previous / 3 : previous
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>いいね！：{count}</p>
+        <p>{text}</p>
+        <button onClick={() => increment()}>count+1</button>
+        <button onClick={() => decrement()}>count-1</button>
+
+        <button onClick={() => increment2()}>count+1</button>
+        <button onClick={() => decrement2()}>count-1</button>
+        <button onClick={() => reset()}>reset</button>
+        <button onClick={() => multiple()}>x2</button>
+        <button onClick={() => three()}>3の倍数の時だけ3でわる</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
