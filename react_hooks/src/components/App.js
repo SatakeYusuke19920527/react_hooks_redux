e5dbf2afd7f14ac916ac1d86fc67341a641b679c
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-02-27 08:30:39 +0900

    component化

--- react_hooks/src/components/App.js
+++ react_hooks/src/components/App.js
@@ -1,6 +1,7 @@
 import React, {useReducer, useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Event from './Event';
 import reducer from '../reducers/index';
 
 function App() {
@@ -10,8 +11,6 @@ function App() {
 
 
   const addEvent = () => {
-    console.log(title);
-    console.log(body);
 
     dispatch({
         type: 'CREATE_EVENT',
@@ -23,7 +22,11 @@ function App() {
     setBody('')
   }
 
-  console.log({state})
+  const deleteAll = () => {
+    dispatch({
+        type: 'DELETE_ALL_EVENT',
+    })
+  }
 
   return (
   <div className="container-fluid">
@@ -39,7 +42,7 @@ function App() {
         </div>
 
         <button type="button" className="btn btn-primary" onClick={() => {addEvent()}}>イベント作成</button>
-        <button type="button" className="btn btn-danger">全てのイベントを削除</button>
+        <button type="button" className="btn btn-danger" onClick={() => {deleteAll()}}>全てのイベントを削除</button>
 
       </form>
       <h4>イベント一覧</h4>
@@ -49,26 +52,13 @@ function App() {
             <th scope="col">ID</th>
             <th scope="col">タイトル</th>
             <th scope="col">ボディー</th>
+            <th scope="col">削除</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colSpan="1">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {state.map((event, index) => (
+            <Event key={index} event={event} dispatch={dispatch} />
+          ))}
         </tbody>
       </table>
     </div>
