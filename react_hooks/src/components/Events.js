081b531a0ca0cadbd9ceba3c184569b6b9a00e1a
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-02-27 12:47:05 +0900

    入門編終了

--- react_hooks/src/components/Events.js
+++ react_hooks/src/components/Events.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Event from './Event';
+import AppContext from '../contexts/AppContext';
 
-const Events = ({state, dispatch}) => {
+const Events = () => {
+    const {state} = useContext(AppContext)
     return (
         <React.Fragment>
-        <h4>イベント一覧</h4>
-        <table className="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">タイトル</th>
-                    <th scope="col">ボディー</th>
-                    <th scope="col">削除</th>
-                </tr>
-            </thead>
-            <tbody>
-                {state.map((event, index) => (
-                    <Event key={index} event={event} dispatch={dispatch} />
-                ))}
-            </tbody>
+            <h4>イベント一覧</h4>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">タイトル</th>
+                        <th scope="col">ボディー</th>
+                        <th scope="col">削除</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {state.map((event, index) => (
+                        <Event key={index} event={event} />
+                    ))}
+                </tbody>
             </table>
         </React.Fragment>
     );
